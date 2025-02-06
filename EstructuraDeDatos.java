68bbc4deb97c1d13cb7fae915c9935c02d0ee658
OscarIvan3 <163325789+OscarIvan3@users.noreply.github.com>
2025-02-06 16:47:17 -0600

    Hola

---
+++ EstructuraDeDatos.java
@@ -0,0 +1,17 @@
+public class EstructuraDeDatos {
+    public static void main(String[] args) {
+        Calificaciones Calificaciones = new Calificaciones();
+        Calificaciones.main(args);
+
+        clase clase = new clase();
+        clase.main (args);
+
+
+        PiedraPapelTijera PiedraPapelTijera = new PiedraPapelTijera();
+        PiedraPapelTijera.main(args);
+
+        UserData UserData = new UserData();
+        System.out.println(UserData.Fullname("Oscar","Aragon"));
+
+    }
+}
---
+++ UserData.java
@@ -0,0 +1,9 @@
+//Oscar Aragon
+//14715
+//UserData
+//05/02/2025
+public class UserData {
+    public String Fullname( String FirstName,String LastName){
+        return String.format("El nombre completo es %s %s", FirstName, LastName);
+    }
+}
